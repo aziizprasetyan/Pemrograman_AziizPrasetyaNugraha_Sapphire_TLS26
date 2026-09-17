f0eab2106de92d8c90dca5e46efd917ff3eebf3e
aziizprasetyan <prasetyaaziiz868@gmail.com>
2026-09-17 11:07:36 +0700

    Add files via upload

---
+++ Problem01TLS26.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+void hapusAstronaut(int astronaut[], int &jumlah, int posisi) {
+    for (int i = posisi; i < jumlah - 1; i++) {
+        astronaut[i] = astronaut[i + 1];
+    }
+
+    jumlah--;
+}
+
+int main() {
+    int N, K;
+
+    cout << "Masukkan jumlah astronaut: ";
+    cin >> N;
+
+    cout << "Masukkan nilai K: ";
+    cin >> K;
+
+    int astronaut[100];
+
+    for (int i = 0; i < N; i++) {
+        astronaut[i] = i + 1;
+    }
+
+    int jumlah = N;
+    int posisi = 0;
+
+    cout << "\nUrutan eliminasi:\n";
+
+    while (jumlah > 1) {
+
+        posisi = (posisi + K - 1) % jumlah;
+
+        int yangDihapus = astronaut[posisi];
+
+        cout << yangDihapus << " ";
+
+        hapusAstronaut(astronaut, jumlah, posisi);
+
+        if (yangDihapus % 2 == 0) {
+            K += 2;
+        } 
+        else {
+            K -= 1;
+
+            if (K < 2) {
+                K = 2;
+            }
+        }
+    }
+
+    cout << "\n\nAstronaut terakhir yang bertahan: "
+         << astronaut[0] << endl;
+
+    return 0;
+}
